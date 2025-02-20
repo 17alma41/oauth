97efd05e175dd3d0e8e3df88295f2fde814fb24b
alvaro <alvarodevejer4@gmail.com>
2025-02-20 12:50:18 +0100

    update

--- server.js
+++ server.js
@@ -2,18 +2,32 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 const app = express();
 const port = 3000;
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 // Configuración de logearte con Github
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+    (accessToken, refreshToken, profile, done) => {
+        return done(null, profile);
+  }
+));
+
+// Configuración de logearte con Google
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
   },
     (accessToken, refreshToken, profile, done) => {
         return done(null, profile);
@@ -47,18 +61,30 @@ function ensureAuthenticated(req, res, next) {
 
 app.get("/", (req, res) => {
     const html = "<a href='/auth/github'>Login with Github</a>";
-    res.send(html);
+    const html2 = "<a href='/auth/google'>Login with Google</a>";
+    res.send(html + "<br>" + html2);
 });
 
 app.get("/auth/github", 
     passport.authenticate("github", { scope: ["user:email"] }
 ));
 
+app.get("/auth/google", 
+    passport.authenticate("google", { scope: ["user:email"] }
+));
+
 app.get("/auth/github/callback", 
     passport.authenticate("github", { failureRedirect: "/" }), //Te redirige a la home page si falla
     (req, res) => {
     res.redirect("/profile");
-    console.log(req);
+    //console.log(req);
+});
+
+app.get("/auth/google/callback", 
+    passport.authenticate("google", { failureRedirect: "/" }), //Te redirige a la home page si falla
+    (req, res) => {
+    res.redirect("/profile");
+    //console.log(req);
 });
 
 app.get("/profile", ensureAuthenticated, (req, res) => {
